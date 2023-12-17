a8366051f2afd537f7ba98c4fc0f9f9fb3cb9703
Gilbert <shikweg@gmail.com>
2023-12-17 17:09:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Somebody";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Somebody else";
+}
+
